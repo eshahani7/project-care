b8a276fa82fa408fd9e546d03032e258b1314a9a
Ekta Shahani <shahani.ekta@gmail.com>
2018-02-01 13:56:29 -0800

    xcode project setup

---
+++ projectCARE WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  projectCARE WatchKit Extension
+//
+//  Created by Ekta Shahani on 2/1/18.
+//  Copyright © 2018 Ekta Shahani. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
