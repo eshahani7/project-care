d311a1b036388d122d7c755361645d17cb021c5a
Ekta Shahani <shahani.ekta@gmail.com>
2018-02-04 13:38:44 -0800

    HealthKit authorization on launch

--- projectCARE/HealthStore.swift
+++ projectCARE/HealthStore.swift
@@ -10,13 +10,50 @@ import Foundation
 import HealthKit
 
 class HealthStore {
-    let store:HKHealthStore?
-    init() {
-        if HKHealthStore.isHealthDataAvailable() {
-            store = HKHealthStore();
-        } else {
-            store = nil;
-            //throw error message
-        }
+    private enum HealthkitSetupError: Error {
+        case notAvailableOnDevice
+        case dataTypeNotAvailable
     }
+    
+    class func authorizeHealthKit(completion: @escaping (Bool, Error?) -> Void) {
+        guard HKHealthStore.isHealthDataAvailable() else {
+            completion(false, HealthkitSetupError.notAvailableOnDevice)
+            return
+        }
+        
+        guard   let dateOfBirth = HKObjectType.characteristicType(forIdentifier: .dateOfBirth),
+                let biologicalSex = HKObjectType.characteristicType(forIdentifier: .biologicalSex),
+                let bodyMassIndex = HKObjectType.quantityType(forIdentifier: .bodyMassIndex),
+                let height = HKObjectType.quantityType(forIdentifier: .height),
+                let bodyMass = HKObjectType.quantityType(forIdentifier: .bodyMass),
+                let activeEnergy = HKObjectType.quantityType(forIdentifier: .activeEnergyBurned),
+                let exerciseTime = HKObjectType.quantityType(forIdentifier: .appleExerciseTime),
+                let stepCount = HKObjectType.quantityType(forIdentifier: .stepCount),
+                let heartRate = HKObjectType.quantityType(forIdentifier: .heartRate),
+                let respRate = HKObjectType.quantityType(forIdentifier: .respiratoryRate) else {
+                
+                    completion(false, HealthkitSetupError.dataTypeNotAvailable)
+                    return
+        }
+        
+        let writeTypes: Set<HKSampleType> = [stepCount,
+                                        HKObjectType.workoutType()]
+        
+        let readTypes: Set<HKObjectType> = [activeEnergy,
+                                            dateOfBirth,
+                                            biologicalSex,
+                                            bodyMassIndex,
+                                            height,
+                                            bodyMass,
+                                            exerciseTime,
+                                            stepCount,
+                                            heartRate,
+                                            respRate,
+                                            HKObjectType.workoutType()]
+        
+        HKHealthStore().requestAuthorization(toShare: writeTypes, read: readTypes, completion: { (success, error) in
+            completion(success, error)
+        })
+    }
+    
 }
--- projectCARE/ViewController.swift
+++ projectCARE/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private func authorizeHealthKit() {
+        HealthStore.authorizeHealthKit { (authorized, error) in
+            
+            guard authorized else {
+                
+                let baseMessage = "HealthKit Authorization Failed"
+                
+                if let error = error {
+                    print("\(baseMessage). Reason: \(error.localizedDescription)")
+                } else {
+                    print(baseMessage)
+                }
+                
+                return
+            }
+            
+            print("HealthKit Successfully Authorized.")
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        authorizeHealthKit()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
