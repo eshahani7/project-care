548b52fa134411278091469536d5cb692de8f195
Ekta Shahani <shahani.ekta@gmail.com>
2018-02-08 14:27:34 -0800

    added hr zones

--- projectCARE/HRCalculations.swift
+++ projectCARE/HRCalculations.swift
@@ -10,12 +10,44 @@ import Foundation
 import HealthKit
 
 class HRCalculations {
-    let maxHR:Int?
+    var maxHR:Int = 0
+    let restingHR:Int?
+    var peakZone:Double = 0
+    var cardioZone:Double = 0
+    var fatBurnZone:Double = 0
+    let outOfZone:Double = 0
     let store:HealthStore?
     
     init(store:HealthStore) {
         self.store = store
         maxHR = 220 - store.getAge()
+        peakZone = Double(maxHR) * 0.85
+        cardioZone = Double(maxHR) * 0.7
+        fatBurnZone = Double(maxHR)  * 0.5
+        //init restingHR by sampling and averaging HR over 24 hours
+        
+    }
+    
+    func inPeakZone(currHR:Double) -> Bool {
+        return currHR >= peakZone
+    }
+    
+    func inCardioZone(currHR:Double) -> Bool {
+        return currHR >= cardioZone && currHR < peakZone
+    }
+    
+    func inFatBurnZone(currHR:Double) -> Bool {
+        return currHR >= fatBurnZone && currHR < cardioZone
+    }
+    
+    func calcBestZone(workoutMins:Int) -> Int {
+        if workoutMins < 20 {
+            return 85;
+        }
+        else if workoutMins > 30 {
+            return 50;
+        }
+        return 70;
     }
     
     
