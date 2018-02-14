1ab02cc0c888d59348467ae15c2f6a7042780145
admin <admin@cavs.local>
2018-02-14 00:03:46 -0800

    Imported SwiftCharts and made a dummy data graph show up on the simulator

--- projectCARE/ViewController.swift
+++ projectCARE/ViewController.swift
@@ -7,12 +7,58 @@
 //
 
 import UIKit
+import SwiftCharts
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let chartConfig = BarsChartConfig(
+            valsAxisConfig: ChartAxisConfig(from: 0, to: 8, by: 2)
+        )
+
+        let frame = CGRect(x: 0, y: 70, width: 300, height: 500)
+
+        let chart = BarsChart(
+            frame: frame,
+            chartConfig: chartConfig,
+            xTitle: "X axis",
+            yTitle: "Y axis",
+            bars: [
+                ("A", 2),
+                ("B", 4.5),
+                ("C", 3),
+                ("D", 5.4),
+                ("E", 6.8),
+                ("F", 0.5)
+            ],
+            color: UIColor.red,
+            barWidth: 20
+        )
+
+        self.view.addSubview(chart.view)
+//
+//        let chartConfig = ChartConfigXY(
+//            xAxisConfig: ChartAxisConfig(from: 2, to: 14, by: 2),
+//            yAxisConfig: ChartAxisConfig(from: 0, to: 14, by: 2)
+//        )
+//
+//        let frame = CGRect(x: 0, y: 70, width: 300, height: 500)
+//
+//        let chart = LineChart(
+//            frame: frame,
+//            chartConfig: chartConfig,
+//            xTitle: "TIME",
+//            yTitle: "STEP COUNT",
+//            lines: [
+//                (chartPoints: [(2.0, 10.6), (4.2, 5.1), (7.3, 3.0), (8.1, 5.5), (14.0, 8.0)], color: UIColor.red),
+//                (chartPoints: [(2.0, 2.6), (4.2, 4.1), (7.3, 1.0), (8.1, 11.5), (14.0, 3.0)], color: UIColor.blue)
+//            ]
+//        )
+//
+//        self.view.addSubview(chart.view)
+        //self.chart = chart
     }
 
     override func didReceiveMemoryWarning() {
