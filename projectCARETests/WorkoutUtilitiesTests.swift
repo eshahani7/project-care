b82a02a123640550592aea51f672f910a0825b8f
Ekta Shahani <shahani.ekta@gmail.com>
2018-02-22 14:39:20 -0800

    unit testing new features

--- projectCARETests/WorkoutUtilitiesTests.swift
+++ projectCARETests/WorkoutUtilitiesTests.swift
@@ -12,12 +12,10 @@ class WorkoutUtilitiesTests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        print("testing")
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        print("done testing")
         super.tearDown()
     }
     
@@ -46,4 +44,4 @@ class WorkoutUtilitiesTests: XCTestCase {
         XCTAssertEqual(round(calsBurned), 114)
     }
     
-}
+ }
