2c40f7088d9b9eb4ec1aa93bba49ae34792465ab
Ekta Shahani <shahani.ekta@gmail.com>
2018-02-21 21:58:05 -0800

    Added unit tests

--- projectCARE/HealthStore.swift
+++ projectCARE/HealthStore.swift
@@ -184,8 +184,19 @@ class HealthStore {
             let calendar = Calendar.current
             let todayDateComponents = calendar.dateComponents([.year],
                                                               from: today)
+            let todayMonthComponents = calendar.dateComponents([.month],
+                                                               from: today)
             let thisYear = todayDateComponents.year!
-            let age = thisYear - (dob?.year!)! - 1
+            let thisMonth = todayMonthComponents.month!
+            
+            let age:Int
+            
+            if (dob?.month)! <= thisMonth {
+                age = thisYear - (dob?.year!)!
+            } else {
+                age = thisYear - (dob?.year!)! - 1
+            }
+            
             return age
         } catch {
             print("can't get dob")
---
+++ projectCARETests/HealthStoreTests.swift
@@ -0,0 +1,137 @@
+//
+//  HealthStoreTests.swift
+//  projectCARE
+//
+//  Created by Ekta Shahani on 2/21/18.
+//  Copyright © 2018 Ekta Shahani. All rights reserved.
+//
+import XCTest
+import HealthKit
+
+class HealthStoreTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testHealthStoreGetInstance() {
+        let store:HealthStore = HealthStore.getInstance()
+        XCTAssertNotNil(store)
+    }
+    
+    func testHealthStoreGetAge() {
+        let store:HealthStore = HealthStore.getInstance()
+        let simulatorAge:Int = 21
+        XCTAssertEqual(store.getAge(), simulatorAge)
+    }
+    
+    func testHealthStoreGetSamples() {
+        let expectation = self.expectation(description: "got samples")
+        let store:HealthStore = HealthStore.getInstance()
+        
+        //---------------------SEEDING----------------------------------//
+        let appleStore:HKHealthStore = HKHealthStore()
+        
+        let steps1 = HKQuantity(unit: HKUnit.count(), doubleValue: 1000)
+        let stepSample1 = HKQuantitySample(type: HealthValues.stepCount!, quantity: steps1, start: Date(), end: Date())
+        
+        appleStore.save(stepSample1) { (success, error) in
+            
+            if let error = error {
+                print("Error Saving step: \(error.localizedDescription)")
+            } else {
+                print("Successfully saved step sample")
+            }
+        }
+        //---------------------SEEDING----------------------------------//
+        
+        store.getSamples(sampleType: HealthValues.stepCount!, startDate: Date.distantPast, endDate: Date()) { (sample, error) in
+            guard let samples = sample else {
+                if let error = error {
+                    print(error)
+                    XCTFail()
+                }
+                return
+            }
+            
+            if samples.count > 0 {
+                expectation.fulfill()
+            } else {
+                XCTFail()
+            }
+        }
+        
+        waitForExpectations(timeout: 5.0, handler: nil)
+    }
+    
+    func testHealthStoreGetWorkouts() {
+        let expectation = self.expectation(description: "got workouts")
+        let store:HealthStore = HealthStore.getInstance()
+        
+        //---------------------SEEDING----------------------------------//
+        let calorieQuantity = HKQuantity(unit: HKUnit.kilocalorie(), doubleValue: 200)
+        let distQuantity = HKQuantity(unit: HKUnit.mile(), doubleValue: 4)
+        
+        let workout = HKWorkout(activityType: .running,
+                                start: Date(),
+                                end: Date(),
+                                duration: 1800,
+                                totalEnergyBurned: calorieQuantity,
+                                totalDistance: distQuantity,
+                                device: HKDevice.local(),
+                                metadata: nil)
+        
+        let appleStore:HKHealthStore = HKHealthStore()
+        appleStore.save(workout) { (success, error) in
+            
+            if let error = error {
+                print("Error Saving step: \(error.localizedDescription)")
+            } else {
+                var samples: [HKQuantitySample] = []
+                let heartRateUnit = HKUnit(from: "count/min")
+                let heartRateQuantity = HKQuantity(unit: heartRateUnit, doubleValue: 95)
+                let heartRateForIntervalSample =
+                    HKQuantitySample(type: HealthValues.heartRate!, quantity: heartRateQuantity,
+                                     start: Date(), end: Date())
+                samples.append(heartRateForIntervalSample)
+                
+                appleStore.add(samples, to: workout) {
+                    (success, error) in
+                    
+                    if let error = error {
+                        print("can't save heart rate")
+                        print(error)
+                    } else {
+                        print("saved heart rate")
+                    }
+                }
+                print("Successfully saved step sample")
+            }
+        }
+        //---------------------SEEDING----------------------------------//
+        
+        store.getWorkouts() { (sample, error) in
+            
+            guard let samples = sample else {
+                if let error = error {
+                    print(error)
+                    XCTFail()
+                }
+                return
+            }
+            if samples.count > 0 {
+                expectation.fulfill()
+            } else {
+                XCTFail()
+            }
+        }
+        
+        waitForExpectations(timeout: 5.0, handler: nil)
+    }
+}
