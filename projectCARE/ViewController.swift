ce45526a3235a5a132b8309493b51f570e10b144
Cindy Lu <cindylu0339@gmail.com>
2018-02-22 00:23:55 -0800

    added segue between table view controller and view controller

--- projectCARE/ViewController.swift
+++ projectCARE/ViewController.swift
@@ -8,6 +8,23 @@
 
 import UIKit
 
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
+}
+
 class ViewController: UIViewController {
 
     let store:HealthStore = HealthStore.getInstance()
@@ -23,7 +40,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-
+    override func viewDidAppear(_ animated: Bool) {
+        
+        // 1
+        let nav = self.navigationController?.navigationBar
+        
+        // 2
+        let deepBlue = UIColor(rgb: 0x293044)
+        let fontColor = UIColor(rgb: 0xE6E6E6)
+        nav?.tintColor = fontColor
+        
+        // 3
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        imageView.contentMode = .scaleAspectFit
+        
+        // 4
+        //let image = UIImage(named: "heart")
+        //imageView.image = image
+        
+        // 5
+        navigationItem.titleView = imageView
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
