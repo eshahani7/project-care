6e41ebc37203e2e53fa55dc1546487b567d9b5af
aditya-nadkarni <nadkarniaditya@gmail.com>
2018-03-19 14:07:02 -0700

    Added watch tests

---
+++ projectCARETests/WatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  WatchTests.swift
+//  projectCARETests
+//
+//  Created by Aditya Nadkarni on 3/19/18.
+//  Copyright © 2018 Ekta Shahani. All rights reserved.
+//
+
+import Foundation
+
+/*****************************************************************************************************************
+ 
+ Watch tests can be found under this git repository. Parses output from projectCARE to ensure everything works.
+ Instructions to run tests, can be found in the README.md of the repo
+ 
+ https://github.com/aditya-nadkarni/project-care-watch-tests
+ *****************************************************************************************************************/
